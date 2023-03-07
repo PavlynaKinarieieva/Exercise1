57a73d01904c3460f41403f2355e2d9ac2df7562
Pavlyna Kinarieieva <pavlynakinarieieva@Air-Pavlyna.fritz.box>
2023-03-07 14:13:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        String string = " Hello, I am Pavlyna";
+        double numberDouble = 1.5;
+        boolean booleanVal = true;
+
+        System.out.println(number);
+        System.out.println(string);
+        System.out.println(numberDouble);
+        System.out.println(booleanVal);
+
+
+
+    }
+}
